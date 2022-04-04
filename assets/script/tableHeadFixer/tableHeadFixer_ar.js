f9e21ee43b66e05878e666100b3083bbae0cfbd9
FaisalSEC <91873705+FaisalSEC@users.noreply.github.com>
2022-04-04 11:16:32 +0300

    1

--- assets/script/tableHeadFixer/tableHeadFixer_ar.js
+++ assets/script/tableHeadFixer/tableHeadFixer_ar.js
@@ -1,20 +1,20 @@
-(function ($)
-{
+(function ($)
+{
 
-    $.fn.tableHeadFixer = function (param)
-    {
-        var defaults = {
+    $.fn.tableHeadFixer = function (param)
+    {
+        var defaults = {
             head: true,
             foot: false,
             left: 0,
             right: 0,
-            'z-index': 0
+            'z-index': 0
         };
 
         var settings = $.extend({}, defaults, param);
 
-        return this.each(function ()
-        {
+        return this.each(function ()
+        {
             settings.table = this;
             
             settings.parent = $(settings.table).parent().eq(0).eq(0);
@@ -37,92 +37,92 @@
 
             $(settings.parent).trigger("scroll");
 
-            $(window).resize(function ()
-            {
-                $(settings.parent).trigger("scroll");
-            });
+            $(window).resize(function ()
+            {
+                $(settings.parent).trigger("scroll");
+            });
         });
 
         /*
 		 This function solver z-index problem in corner cell where fix row and column at the same time,
 		 set corner cells z-index 1 more then other fixed cells
 		 */
-        function setCorner()
-        {
+        function setCorner()
+        {
             var table = $(settings.table);
 
-            if (settings.head)
-            {
-                if (settings.left > 0)
-                {
+            if (settings.head)
+            {
+                if (settings.left > 0)
+                {
                     var tr = table.find("thead tr");
 
-                    tr.each(function (k, row)
-                    {
-                        solverLeftColspan(row, function (cell)
-                        {
-                            $(cell).css("z-index", settings['z-index'] + 1);
-                        });
-                    });
+                    tr.each(function (k, row)
+                    {
+                        solverLeftColspan(row, function (cell)
+                        {
+                            $(cell).css("z-index", settings['z-index'] + 1);
+                        });
+                    });
                 }
 
-                if (settings.right > 0)
-                {
+                if (settings.right > 0)
+                {
                     var tr = table.find("thead tr");
 
-                    tr.each(function (k, row)
-                    {
-                        solveRightColspan(row, function (cell)
-                        {
-                            $(cell).css("z-index", settings['z-index'] + 1);
-                        });
-                    });
-                }
+                    tr.each(function (k, row)
+                    {
+                        solveRightColspan(row, function (cell)
+                        {
+                            $(cell).css("z-index", settings['z-index'] + 1);
+                        });
+                    });
+                }
             }
 
-            if (settings.foot)
-            {
-                if (settings.left > 0)
-                {
+            if (settings.foot)
+            {
+                if (settings.left > 0)
+                {
                     var tr = table.find("tfoot tr");
 
-                    tr.each(function (k, row)
-                    {
-                        solverLeftColspan(row, function (cell)
-                        {
-                            $(cell).css("z-index", settings['z-index']);
-                        });
-                    });
+                    tr.each(function (k, row)
+                    {
+                        solverLeftColspan(row, function (cell)
+                        {
+                            $(cell).css("z-index", settings['z-index']);
+                        });
+                    });
                 }
 
-                if (settings.right > 0)
-                {
+                if (settings.right > 0)
+                {
                     var tr = table.find("tfoot tr");
 
-                    tr.each(function (k, row)
-                    {
-                        solveRightColspan(row, function (cell)
-                        {
-                            $(cell).css("z-index", settings['z-index']);
-                        });
-                    });
-                }
-            }
+                    tr.each(function (k, row)
+                    {
+                        solveRightColspan(row, function (cell)
+                        {
+                            $(cell).css("z-index", settings['z-index']);
+                        });
+                    });
+                }
+            }
         }
         // Set style of table parent
-        function setParent()
-        {
-            
+        function setParent()
+        {
+            
             var parent = $(settings.parent);
             var table = $(settings.table);
 
             parent.append(table);
             parent
-				.css({
+				.css({
 				    'overflow-x': 'auto',
-				    'overflow-y': 'auto'
+				    'overflow-y': 'auto'
 				});
-            parent.scroll(function ()
+            parent.scroll(function ()
             {
                 var scrollWidth = parent[0].scrollWidth;
                 var clientWidth = parent[0].clientWidth;
@@ -141,44 +141,44 @@
                     settings.leftColumns.css("left", left);
 
                 if (settings.right > 0)
-                {
-                    console.log("columns");
-
-                    console.log(settings.rightColumns);
-                    settings.rightColumns.css("right", scrollWidth - clientWidth - left);
-                }
-            }.bind(table));
+                {
+                    console.log("columns");
+
+                    console.log(settings.rightColumns);
+                    settings.rightColumns.css("right", scrollWidth - clientWidth - left);
+                }
+            }.bind(table));
         }
 
         // Set table head fixed
-        function fixHead()
-        {
+        function fixHead()
+        {
             var thead = $(settings.table).find("thead");
             var tr = thead.find("tr");
             var cells = thead.find("tr > *");
 
             setBackground(cells);
-            cells.css({
-                'position': 'relative'
-            });
+            cells.css({
+                'position': 'relative'
+            });
         }
 
         // Set table foot fixed
-        function fixFoot()
-        {
+        function fixFoot()
+        {
             var tfoot = $(settings.table).find("tfoot");
             var tr = tfoot.find("tr");
             var cells = tfoot.find("tr > *");
 
             setBackground(cells);
-            cells.css({
-                'position': 'relative'
-            });
+            cells.css({
+                'position': 'relative'
+            });
         }
 
         // Set table left column fixed
-        function fixLeft()
-        {
+        function fixLeft()
+        {
             var table = $(settings.table);
 
             // var fixColumn = settings.left;
@@ -186,12 +186,12 @@
             settings.leftColumns = $();
 
             var tr = table.find("tr");
-            tr.each(function (k, row)
-            {
+            tr.each(function (k, row)
+            {
 
-                solverLeftColspan(row, function (cell)
-                {
-                    settings.leftColumns = settings.leftColumns.add(cell);
+                solverLeftColspan(row, function (cell)
+                {
+                    settings.leftColumns = settings.leftColumns.add(cell);
                 });
                 // var inc = 1;
 
@@ -209,21 +209,21 @@
 
             var column = settings.leftColumns;
 
-            column.each(function (k, cell)
-            {
+            column.each(function (k, cell)
+            {
                 var cell = $(cell);
 
                 setBackground(cell);
-                cell.css({
-                    'position': 'relative'
-                });
-            });
+                cell.css({
+                    'position': 'relative'
+                });
+            });
         }
 
         // Set table right column fixed
-        function fixRight()
-        {
-            
+        function fixRight()
+        {
+            
             var table = $(settings.table);
 
             var fixColumn = settings.right;
@@ -231,33 +231,33 @@
             settings.rightColumns = $();
 
             var tr = table.find("tr");
-            tr.each(function (k, row)
-            {
-                solveRightColspan(row, function (cell)
-                {
-                    settings.rightColumns = settings.rightColumns.add(cell);
-                });
+            tr.each(function (k, row)
+            {
+                solveRightColspan(row, function (cell)
+                {
+                    settings.rightColumns = settings.rightColumns.add(cell);
+                });
             });
 
             var column = settings.rightColumns;
 
-            column.each(function (k, cell)
-            {
+            column.each(function (k, cell)
+            {
                 var cell = $(cell);
 
                 setBackground(cell);
-                cell.css({
-                    'position': 'relative'
-                });
-            });
+                cell.css({
+                    'position': 'relative'
+                });
+            });
 
         }
 
         // Set fixed cells backgrounds
-        function setBackground(elements)
-        {
-            elements.each(function (k, element)
-            {
+        function setBackground(elements)
+        {
+            elements.each(function (k, element)
+            {
                 var element = $(element);
                 var parent = $(element).parent();
 
@@ -270,37 +270,37 @@
                 var background = parentBackground ? parentBackground : "white";
                 background = elementBackground ? elementBackground : background;
 
-                element.css("background-color", background);
-            });
+                element.css("background-color", background);
+            });
         }
 
-        function solverLeftColspan(row, action)
-        {
+        function solverLeftColspan(row, action)
+        {
             var fixColumn = settings.left;
             var inc = 1;
 
-            for (var i = 1; i <= fixColumn; i = i + inc)
-            {
+            for (var i = 1; i <= fixColumn; i = i + inc)
+            {
                 var nth = inc > 1 ? i - 1 : i;
 
                 var cell = $(row).find("> *:nth-child(" + nth + ")");
                 var colspan = cell.prop("colspan");
 
-                if (cell.cellPos().left < fixColumn)
-                {
-                    action(cell);
+                if (cell.cellPos().left < fixColumn)
+                {
+                    action(cell);
                 }
 
-                inc = colspan;
-            }
+                inc = colspan;
+            }
         }
-        function solveRightColspan(row, action)
-        {
+        function solveRightColspan(row, action)
+        {
             var fixColumn = settings.right;
             var inc = 1;
 
-            for (var i = 1; i <= fixColumn; i = i + inc)
-            {
+            for (var i = 1; i <= fixColumn; i = i + inc)
+            {
                 var nth = inc > 1 ? i - 1 : i;
 
                 var cell = $(row).find("> *:nth-last-child(" + nth + ")");
@@ -308,10 +308,10 @@
 
                 action(cell);
 
-                inc = colspan;
-            }
-        }
-    };
+                inc = colspan;
+            }
+        }
+    };
 
 })(jQuery);
 
@@ -324,16 +324,16 @@
             $(this).text( $(this).cellPos().top +", "+ $(this).cellPos().left );
         });
 */
-(function ($)
+(function ($)
 {
     /* scan individual table and set "cellPos" data in the form { left: x-coord, top: y-coord } */
-    function scanTable($table)
-    {
+    function scanTable($table)
+    {
         var m = [];
-        $table.children("tr").each(function (y, row)
-        {
-            $(row).children("td, th").each(function (x, cell)
-            {
+        $table.children("tr").each(function (y, row)
+        {
+            $(row).children("td, th").each(function (x, cell)
+            {
                 var $cell = $(cell),
                     cspan = $cell.attr("colspan") | 0,
                     rspan = $cell.attr("rowspan") | 0,
@@ -341,34 +341,34 @@
                 cspan = cspan ? cspan : 1;
                 rspan = rspan ? rspan : 1;
                 for (; m[y] && m[y][x]; ++x);  //skip already occupied cells in current row
-                for (tx = x; tx < x + cspan; ++tx)
+                for (tx = x; tx < x + cspan; ++tx)
                 {  //mark matrix elements occupied by current cell with true
-                    for (ty = y; ty < y + rspan; ++ty)
-                    {
-                        if (!m[ty])
+                    for (ty = y; ty < y + rspan; ++ty)
+                    {
+                        if (!m[ty])
                         {  //fill missing rows
-                            m[ty] = [];
+                            m[ty] = [];
                         }
-                        m[ty][tx] = true;
-                    }
+                        m[ty][tx] = true;
+                    }
                 }
                 var pos = { top: y, left: x };
-                $cell.data("cellPos", pos);
-            });
-        });
+                $cell.data("cellPos", pos);
+            });
+        });
     };
 
     /* plugin */
-    $.fn.cellPos = function (rescan)
-    {
+    $.fn.cellPos = function (rescan)
+    {
         var $cell = this.first(),
             pos = $cell.data("cellPos");
-        if (!pos || rescan)
-        {
+        if (!pos || rescan)
+        {
             var $table = $cell.closest("table, thead, tbody, tfoot");
-            scanTable($table);
+            scanTable($table);
         }
         pos = $cell.data("cellPos");
-        return pos;
-    }
+        return pos;
+    }
 })(jQuery);
